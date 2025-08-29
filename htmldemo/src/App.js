948ecffad91996a90abf59800d823288357452ed
KL2400030596 <2400030596@kluniversity.in>
2025-08-29 09:54:45 +0530

    this is htmldemo folder

---
+++ htmldemo/src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <strong>welcome</strong>
+      <i>welcome</i><br/>
+      <b>welcome</b><br/>
+      <ins>wlecome</ins><br/>
+      <del>welcome</del><br/>
+      <em>welcome</em><br/>
+      <h1>weclome to <sup>react project</sup></h1><br/>
+      <h1>welcome to <sub>react project</sub></h1><br/>
+      <mark>welcome</mark><br/>
+      <u>welcome</u><br/>
+      <h2>welcome to react with npx</h2><br/>
+      <h2>wlecome</h2><br/>
+    </div>
+  
+  );
+}
+
+export default App;
